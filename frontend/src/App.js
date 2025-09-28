3e9424b457b2e8a0469a01e72c9af74c30281e08
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2025-09-28 12:45:13 +0300

    feat: Add login and registration routes and enable in-app routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import AuthProvider from './context/AuthContext';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <AuthProvider>
+        <Routes>
+            <Route path='/' element={<Homepage/>}/>
+            <Route path='/auth/login' element={<LoginPage/>}/>
+            <Route path='/auth/register' element={<RegisterPage/>}/>
+        </Routes>
+      </AuthProvider>
+      </>
   );
 }
 
