ec8f2e19ee4f004f51c6fd88ea02bd77764a0b5b
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2026-03-01 20:38:29 +0200

    feat(routes): Use middleware to protect expense-related routes from unauthenticated users

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -1,18 +1,21 @@
-import express from 'express';
-import { getAllUserExpenses, addUserExpense, editUserExpense, deleteUserExpense, deleteAllExpenses } from '../controllers/expense.js';
+import {
+  getAllUserExpenses,
+  addUserExpense,
+  editUserExpense,
+  deleteUserExpense,
+} from "../controllers/expense.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
+import express from "express";
 const router = express.Router();
 
-// Get all user expenses route: handles GET requests to /expense
-router.get('/', getAllUserExpenses)
+// Get all user expenses route: handles GET requests to /expense/expenses
+router.get("/expenses", authMiddleware, getAllUserExpenses);
 // Add user expense route: handles POST requests to /expense
-router.post('/', addUserExpense);
+router.post("/", authMiddleware, addUserExpense);
 // Edit user expense route: handles PATCH requests to /expense
-router.patch('/', editUserExpense);
-// Delete user expense route: handles DELETE requests to /expense
-router.delete('/', deleteUserExpense);
-
-router.delete('/all', deleteAllExpenses)
+router.patch("/", authMiddleware, editUserExpense);
+// Delete user expense route: handles DELETE requests to /expense/:id
+router.delete("/:id", authMiddleware, deleteUserExpense);
 
 export default router;
-
