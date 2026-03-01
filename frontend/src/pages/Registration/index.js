c94465391156fa41e2b5822ea7c08eef1b02dfb9
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2026-03-01 21:42:57 +0200

    style: refactor registration page to match login layout and utility classes

--- frontend/src/pages/Registration/index.js
+++ frontend/src/pages/Registration/index.js
@@ -1,50 +1,47 @@
 import { Link } from 'react-router-dom';
-// Components
-import RegistrationForm from './RegistrationForm';
-// Hooks
 import useRedirectIfAuthenticated from '../../hooks/useRedirectIfAuthenticated';
-// Global CSS styles.
+
+import RegistrationForm from './RegistrationForm';
+
+import piggybank from '../../assets/piggybank.png';
 import '../../styles/forms.css';
 import '../../styles/images.css';
 import '../../styles/layout.css';
 import '../../styles/typography.css';
-// Page-specific CSS styles.
-import './Registration.css';
-// Images
-import piggybank from '../../assets/piggybank.png';
 
 const RegistrationPage = () => {
-  // A hook that checks if the user is already authenticated. If he is, he will be redirected to dashboard page.
+  // Redirect to dashboard page if already logged in.
   useRedirectIfAuthenticated('/dashboard');
 
   return (
-  <main className='g_flex-container g_height-90'>
+    <main className="g_items-in-a-row g_items-in-a-row--flex-wrap--nowrap g_height-90">
 
-    {/* Left section of the page */}
-    <section className='g_left-section'>    
+      <section className="g_left-section">
+        <div>
+          <div className="g_text-center">
+            <h2 className="g_title">Create Account</h2>
+            <p className="g_subtitle">Join us and start managing your budget today.</p>
+          </div>
 
-      <div className="contentBox">
+          <RegistrationForm />
 
-        <div className="g_text-center">
-          <h2 className="g_title">Welcome</h2>
-          <p className="g_subtitle">Let's get started</p>
+          <div className='g_page-footer-link'>
+            Already have an account?{' '}
+            <Link to="/login" className='g_link g_link--color--black'>
+              Login
+            </Link>
+          </div>
         </div>
+      </section>
 
-        {/* Registration form component where the user inputs account details. */}
-        <RegistrationForm />
-
-          {/* Navigation link for users who already have an account. */}
-        <div> Already have an account? <Link to="/login" className="g_link">Login</Link> </div>
-
-      </div>
-    </section>
-
-    {/* Right section of the page */}
-    <section className='g_right-section'>
-      <img src={piggybank} alt="A piggy bank" className="g_width-50 g_floating-image" />
-    </section>
-
-  </main>
+      <section className="g_right-section">
+        <img 
+          src={piggybank} 
+          alt="A piggy bank" 
+          className="g_floating-image" 
+        />
+      </section>
+    </main>
   );
 };
 
