cf38b53252cb6868caab3a6c6ee6068df488e751
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2026-03-01 18:24:32 +0200

    feat: Add DashboardActions component

---
+++ frontend/src/pages/Dashboard/DashboardActions.jsx
@@ -0,0 +1,54 @@
+import expense from '../../assets/expense.png';
+import goal from '../../assets/goal.png';
+import addBudget from '../../assets/addBudget.png';
+import yourGoals from '../../assets/yourGoals.png';
+
+import '../../styles/links_and_buttons.css'
+import '../../styles/actions.css';
+
+/**
+ * DashboardActions component.
+ *
+ * Renders action buttons for the user dashboard depending on whether
+ * the user has a budget.
+ *
+ * @param {Object} props
+ * @param {boolean} props.hasBudget - Indicates if the user has an active budget.
+ * @param {function} props.onAddBudgetClick - Callback when "Add Budget" button is clicked.
+ * @param {function} props.onAddGoalClick - Callback when "Add Goal" button is clicked.
+ * @param {function} props.onYourGoalsClick - Callback when "Your Goals" button is clicked.
+ * @param {function} props.onAddExpenseClick - Callback when "Add Expense" button is clicked.
+ */
+const DashboardActions = ({hasBudget, onAddBudgetClick, onAddGoalClick, onYourGoalsClick, onAddExpenseClick}) => {
+  return (
+    <section className='g_actions-container'>
+      {/* If the user doesn't have a budget, only show the Add Budget button */}
+      {!hasBudget ? (
+        <div className='g_items-in-a-row g_items-in-a-row--justify-content--center'>
+          <button className="g_action-btn" onClick={onAddBudgetClick}>
+            <img src={addBudget} className='g_action__icon g_action__icon--big' alt="Add budget" />
+            <span>Add Budget</span>
+          </button>
+        </div>
+      ) : (
+        <div className='g_actions-grid'>
+          {[
+            // Rendering action buttons
+            { icon: goal, label: "Add Goal", onClick: onAddGoalClick, alt: "Add goal" },
+            { icon: yourGoals, label: "Your Goals", onClick: onYourGoalsClick, alt: "View your goals" },
+            { icon: expense, label: "Add Expense", onClick: onAddExpenseClick, alt: "Add expense", disabled: !hasBudget }
+          ].map(({icon, label, onClick, alt, disabled}) => (
+            <button key={label} className='g_action-btn' onClick={onClick} disabled={disabled} title={disabled ? "Add a budget first" : ""}>
+              {/* Button icon */}
+              <img src={icon} className='g_action__icon g_action__icon--big' alt={alt} />
+              {/* Button label */}
+              <span>{label}</span>
+            </button>
+          ))}
+        </div>
+      )}
+    </section>
+      )
+}
+
+export default DashboardActions;
